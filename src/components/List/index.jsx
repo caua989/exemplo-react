a0b5fbacd5506ff2b2638f6f690d04b51474d249
cauã <caua62183626@alunos.sc.senac.br>
2026-03-30 22:14:12 -0300

    Fim da primeira aula

---
+++ src/components/List/index.jsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+function List () {
+    return (
+   <section>
+    <h2 id="exemplo-h2">Seção de Listas</h2>
+
+    <h3>Lista não ordenada</h3>
+    <ul id="ul">
+      <li>Batata</li>
+      <li>Pao</li>
+      <li>Arroz</li>
+    </ul>
+
+    <h3>Lista ordenada</h3>
+    <ol>
+      <li>Rotiwels</li>
+      <li>Barbye</li>
+      <li>Poquemon</li>
+    </ol>
+  </section>
+    )
+}
+
+export default List
